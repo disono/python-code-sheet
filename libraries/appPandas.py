cb22ce2dc5c97f6f3c832c5d041dac3a49ae0a75
Achie Disono <disono.apps@gmail.com>
2020-11-02 07:55:58 +0800

    Add concat on dataframes

--- libraries/appPandas.py
+++ libraries/appPandas.py
@@ -57,3 +57,17 @@ x = df.groupby('items')
 #print(x.mean())
 #print(x.count())
 print(x.describe().transpose())
+# joining
+x1 = {'a': [1,2,3],'b':[4,5,6]}
+y1 = {'c': [7,8,9],'d':[10,11,12]}
+x = pd.DataFrame(x1, index = ['p1', 'p2', 'p3'])
+y = pd.DataFrame(y1, index = ['p1', 'p4', 'p5'])
+print(x.join(y, how = 'right'))
+# concatinating
+x1 = {'a': [1,2,3,4,5], 'b': [1,2,3,4,5], 'c': [1,2,3,4,5], 'd': [1,2,3,4,5], 'e': [1,2,3,4,5]}
+x2 = {'e': [1,2,32,4,55], 'f': [1,2,2,2,5], 'h': [1,2,3,4,5], 'i': [1,2,3,4,5], 'j': [11,23,3,4,5]}
+x3 = {'a': [1,2,3,5,5], 'b': [1,2,3,4,5], 'c': [1,3,3,3,5], 'd': [4,4,3,4,5], 'e': [1,2,3,4,5]}
+df1 = pd.DataFrame(x1, index = [1,2,3,4,5])
+df2 = pd.DataFrame(x2, index = [1,2,3,4,5])
+df3 = pd.DataFrame(x3, index = [6,7,8,9,10])
+print(pd.concat([df1,df2,df3], axis = 0))
